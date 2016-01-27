73d53b532e3e587382d53b8539d357b11e811a4b
Franklin Wei <git@fwei.tk>
2016-01-26 21:00:53 -0500

    add travis tests

---
+++ tests/gen_data.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+sleep .1
+echo test
+sleep .1
+echo test
+sleep 3
+echo say Running automated tests...
+sleep .1
+echo take sword
+sleep .1
+echo go in
+sleep .1
+echo client list
+sleep .1
+echo user list
+sleep .1
+echo user add test2
+sleep .1
+echo blah
+sleep .1
+echo blah
+sleep .1
+echo y
+sleep 3
+echo user list
+sleep .1
+echo drop sword
+sleep .1
+echo say Done with tests
+sleep .1
+echo quit
